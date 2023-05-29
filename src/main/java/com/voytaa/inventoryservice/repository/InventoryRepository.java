1cfcb83530b72f2c8d322a7847ce124d557b2b39
Jan Wojtach <wojtachjanek@gmail.com>
2023-05-29 19:12:05 +0200

    Initial commit

---
+++ src/main/java/com/voytaa/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,13 @@
+package com.voytaa.inventoryservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.voytaa.inventoryservice.model.Inventory;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
+
+}
